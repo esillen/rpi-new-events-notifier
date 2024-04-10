67ff3948203440b6758b6a5a23da8e496af310b9
Erik Sillén <erik.sillen@flowertech.se>
2024-04-10 16:42:18 +0200

    wav -> mp3

--- common.py
+++ common.py
@@ -1,6 +1,6 @@
-AUDIO_CLIPS = ['old_person.mp3', 
+AUDIO_CLIPS = ['sboe.mp3', 
+               'so_excited.mp3', 
+               'good_news.mp3',
                'coin.mp3.ogg', 
-               'sboe.wav', 
-               'so_excited.wav', 
-               'good_news.wav',
+               'old_person.mp3',
                'applause.mp3']
