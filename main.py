3b02f8f752fa63e0f3681aa620d05abd50bda17a
Erik Sillén <erik.sillen@flowertech.se>
2024-04-12 10:31:15 +0200

    added polling from dev as well

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ NOTIFICATION_TIME_SECONDS = 15
 POLL_TIME_SECONDS = 10
 PIN = 24
 URL = 'http://mysite.com/event_endpoint'
+URL_DEV = "http://localhost:5000/event_endpoint"
 if 'RPI_EVENT_URL' in os.environ:
     URL = os.environ['RPI_EVENT_URL']
+if 'RPI_EVENT_URL_DEV' in os.environ:
+    URL_DEV = os.environ['RPI_EVENT_URL_DEV']    
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN, GPIO.OUT)
 GPIO.output(PIN, GPIO.HIGH)
-
-lastHash = ""
-
 
 def play_random_audio():
     random_clip = random.choice(AUDIO_CLIPS)
@@ -38,6 +38,13 @@ def notify():
     sleep(NOTIFICATION_TIME_SECONDS)
     GPIO.output(PIN, GPIO.HIGH)
 
+
+# Get initial hashes
+lastHash = requests.get(URL).text
+lastHashDev = requests.get(URL_DEV).text
+print("hash", lastHash)
+print("hashDev", lastHashDev)
+
 # Just notifying that it has started and is up and hasn't run into a problem yet
 print("starting")
 GPIO.output(PIN, GPIO.LOW)
@@ -46,14 +53,15 @@ GPIO.output(PIN, GPIO.HIGH)
 
 while True:
     try:
-        response = requests.get(URL)
-        hash = response.text
-        if hash != lastHash:
+        hash = requests.get(URL).text
+        print("hash", lastHash)
+        hashDev = requests.get(URL_DEV).text
+        print("hashDev", lastHashDev)
+        if hash != lastHash or hashDev != lastHashDev:
             print("new hash!")
-            print(hash)
-            if (lastHash != ""):
-                notify()
+            notify()
             lastHash = hash
+            lastHashDev = hashDev
     except:
         pass
     sleep(POLL_TIME_SECONDS)
