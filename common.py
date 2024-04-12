5c65264b1520ecbc10122329ce3a66f12f255c59
Erik Sillén <erik.sillen@flowertech.se>
2024-04-12 11:44:46 +0200

    added more sounds

--- common.py
+++ common.py
@@ -3,4 +3,9 @@ AUDIO_CLIPS = ['sboe.mp3',
                'good_news.mp3',
                'coin.mp3.ogg', 
                'old_person.mp3',
-               'applause.mp3']
+               'applause.mp3',
+               'mlg-airhorn.mp3',
+               'yeah-boiii-i-i-i.mp3',
+               'george-micael-wham-careless-whisper-1.mp3',
+               'kids_cheering.mp3',
+               'epicsaxguy.mp3']
--- test_audio.py
+++ test_audio.py
@@ -6,15 +6,17 @@ from common import AUDIO_CLIPS
 
 pygame.mixer.init(buffer=1024)
 pygame.mixer.music.set_volume(1.0)
+voice = pygame.mixer.Channel(0)
 
 
 def play_audio(clip):
     filepath = os.path.join(os.path.dirname(__file__), 'clips', clip)
-    pygame.mixer.music.load(filepath)
-    pygame.mixer.music.play()
+    sound = pygame.mixer.Sound(filepath)
+    voice.play(sound)
 
 for clip in AUDIO_CLIPS:
     print(clip)
     play_audio(clip)
-    sleep(7)
-print()
+    while voice.get_busy():
+        pass
+print("done")
